cf44e419ef6045a30ef2fe4657b5731c6fe897ea
dmc mcc <mccarthydonal@hotmail.com>
2024-02-29 14:25:18 +0000

    add views

--- blog/views.py
+++ blog/views.py
@@ -7,9 +7,12 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
 #    model = Post
+#    queryset = Post.objects.filter(author=2)
     queryset = Post.objects.all()
     template_name = "post_list.html"
-
+ #   queryset = Post.objects.all().order_by("created_on")
+ #   queryset = Post.objects.filter(status=1)
+ 
 
 #def my_blog(request):
  #   return HttpResponse("Hello, Blog!")
