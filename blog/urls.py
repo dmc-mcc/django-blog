85c11d74a86cee9e36c25d03c8a69322a5f2f6f5
dmc mcc <mccarthydonal@hotmail.com>
2024-03-01 17:58:40 +0000

    Create the About app

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+#    path('<int:event_id>/', views.post_detail, name='post_detail'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
